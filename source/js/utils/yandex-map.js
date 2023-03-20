f8293046ae0b56206bbd5e013fad95af8e997c93
Valera Komissarenko <nuberman88@gmail.com>
2023-03-20 21:36:27 +0400

    fixed ya-map-js, improved perfomance

--- source/js/utils/yandex-map.js
+++ source/js/utils/yandex-map.js
@@ -1,32 +1,47 @@
-const INITIAL_LATITUDE = 59.938631;
-const INITIAL_LONGITUDE = 30.323037;
-const ZOOM = 16;
+const myOwnScriptElement = document.createElement('script');
+myOwnScriptElement.src = 'https://api-maps.yandex.ru/2.1/?apikey=&lang=ru_RU';
+const mapElement = document.querySelector('#map');
+let switcher = false;
+mapElement.style.display = 'block';
 
-const ymaps = window.ymaps;
+window.addEventListener('scroll', activateMapOnScroll);
 
-const map = document.querySelector('#map');
-
-if (map) {
-  map.style.display = 'block';
-
-  ymaps.ready(function () {
-    let myMap = new ymaps.Map('map', {
-      center: [INITIAL_LATITUDE, INITIAL_LONGITUDE],
-      zoom: ZOOM,
-    }, {
-      searchControlProvider: 'yandex#search',
-    });
-
-    let myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-      hintContent: 'Метка',
-      balloonContent: 'г. Санкт Петербург, ул. Большая Конюшенная, 19/8',
-    }, {
-      iconLayout: 'default#image',
-      iconImageHref: './img/svg/map-marker.svg',
-      iconImageSize: [18, 22],
-      iconImageOffset: [-9, -22],
-    });
-
-    myMap.geoObjects.add(myPlacemark);
+function initYandexMap() {
+  let myMap = new ymaps.Map('map', {
+    center: [59.938631, 30.323037],
+    zoom: 16,
   });
+  let myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+    hintContent: 'Метка',
+    balloonContent: 'г. Санкт Петербург, ул. Большая Конюшенная, 19/8',
+  }, {
+    iconLayout: 'default#image',
+    iconImageHref: './img/svg/map-marker.svg',
+    iconImageSize: [18, 22],
+    iconImageOffset: [-9, -22],
+  });
+  myMap.geoObjects.add(myPlacemark);
 }
+
+const insertApiToHead = () => {
+  document.head.appendChild(myOwnScriptElement);
+};
+
+const initMap = () => {
+  ymaps.ready(initYandexMap);
+};
+
+function activateMapOnScroll() {
+  if (mapElement.getBoundingClientRect().top - window.innerHeight < 2000 && !switcher) {
+    switcher = true;
+    insertApiToHead();
+    myOwnScriptElement.addEventListener('load', () => {
+      if (typeof ymaps !== 'undefined') {
+        initMap();
+      }
+    });
+    window.removeEventListener('scroll', activateMapOnScroll);
+  }
+}
+
+activateMapOnScroll();
