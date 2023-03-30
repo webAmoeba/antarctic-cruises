c8328209c697b557f359cd235e4ff77f9ff6443c
Valera Komissarenko <nuberman88@gmail.com>
2023-03-30 09:56:52 +0400

    fixed testing errors

--- source/js/utils/menu-toggle.js
+++ source/js/utils/menu-toggle.js
@@ -2,6 +2,9 @@ const mainHeader = document.querySelector('.main-header');
 const menuToggler = document.querySelector('.main-header__menu-toggle');
 const navigatorMenu = document.querySelector('.main-header__nav');
 const menu = document.querySelector('.main-header__first-line');
+const overlayMobileMenu = menu.querySelector('.main-header__mobile-overlay');
+const bodyElement = document.body;
+const menuItem = document.querySelectorAll('.main-header__nav-item');
 
 mainHeader.classList.add('main-header__navigation--hidden');
 
@@ -12,14 +15,30 @@ navigatorMenu.classList.add('main-header__nav--mobile-hidden');
 menu.classList.add('main-header__first-line--hidden');
 menu.classList.add('main-header__first-line--moving');
 
+const closeMenu = () => {
+  mainHeader.classList.remove('main-header--mobile-with-js-open');
+  navigatorMenu.classList.add('main-header__nav--mobile-hidden');
+  menuToggler.classList.remove('main-header__menu-toggle--opened');
+  bodyElement.style.overflow = 'unset';
+};
+
 menuToggler.addEventListener('click', () => {
   if (navigatorMenu.classList.contains('main-header__nav--mobile-hidden')) {
     mainHeader.classList.add('main-header--mobile-with-js-open');
     navigatorMenu.classList.remove('main-header__nav--mobile-hidden');
     menuToggler.classList.add('main-header__menu-toggle--opened');
+    bodyElement.style.overflow = 'hidden';
   } else {
-    mainHeader.classList.remove('main-header--mobile-with-js-open');
-    navigatorMenu.classList.add('main-header__nav--mobile-hidden');
-    menuToggler.classList.remove('main-header__menu-toggle--opened');
+    closeMenu();
   }
+});
+
+overlayMobileMenu.addEventListener('click', () => {
+  closeMenu();
+});
+
+menuItem.forEach((el) => {
+  el.addEventListener('click', () => {
+    closeMenu();
+  });
 });
